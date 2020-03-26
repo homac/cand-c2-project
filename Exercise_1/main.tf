d70bb1b400fafa87a8d1018eaaf03289a6a78f7c
Holger Macht <hmacht@emlix.com>
2020-03-26 22:05:47 +0100

    implement exercise 1

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,28 @@
-# TODO: Designate a cloud provider, region, and credentials
+# DONE: Designate a cloud provider, region, and credentials
+provider "aws" {
+  region = "us-east-1"
+  shared_credentials_file = "~/.aws/credentials"
+  profile = "default"
+}
 
+# DONE: provision 4 AWS t2.micro EC2 instances named Udacity T2
+resource "aws_instance" "Udacity_T2" {
+  ami = "ami-0fc61db8544a617ed"
+  instance_type = "t2.micro"
+  count = 4
 
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
+# DONET: provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "Udacity_M4" {
+  ami = "ami-0fc61db8544a617ed"
+  instance_type = "m4.large"
+  count = 2
 
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+  tags = {
+    Name = "Udacity M4"
+  }
+}
